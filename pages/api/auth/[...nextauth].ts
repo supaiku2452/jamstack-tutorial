d48184f4d274ce695f9d48e52459e143d142be47
supaiku2452 <supaiku2452@gmail.com>
2021-05-11 23:20:29 +0900

    added google auth.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from ".prisma/client";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import Adapters from "next-auth/adapters";
 import Providers from "next-auth/providers";
